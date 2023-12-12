aad8f2f8d3108b61a1c85629e8bda90efde5bc1d
slekup <74949101+slekup@users.noreply.github.com>
2023-12-12 21:24:46 +1300

    update setup script

--- setup.sh
+++ setup.sh
@@ -12,9 +12,12 @@ export INFO="${BLUE}INFO${RESET} "
 export DONE="${GREEN}DONE${RESET} "
 export FAIL="${RED}FAIL${RESET} "
 
-# Sync package databasde
+# Enable the "exit on error" option, so that if any command fails, the script will exit
+set -e
+
+# Sync package database
 echo -e "\n${INFO}Updating package database..."
-sudo pacman -Syu || echo -e "${FAIL}Failed to update package database"
+sudo pacman -Syu --noconfirm --needed || { echo -e "${FAIL}Failed to update package database"; exit 1; }
 echo -e "${DONE}Package database updated successfully"
 
 # Install packages with pacman
@@ -28,14 +31,24 @@ while IFS= read -r line; do
       
       # Install the package
       echo "Installing $package - $comment"
-      sudo pacman -S --no-confirm --needed "$package" || echo -e "${FAIL}Failed to install repo packages"
+      sudo pacman -S --noconfirm --needed "$package" || { echo -e "${FAIL}Failed to install repo packages"; exit 1; }
   fi
 done < ./lists/pkg
 echo -e "${DONE}Installed repo packages successfully"
 
 echo -e "${INFO}Registering fonts..."
-fc-cache -f -v || echo -e "${FAIL}Failed to register fonts"
+fc-cache -f -v || { echo -e "${FAIL}Failed to register fonts"; exit 1; }
 echo -e "${DONE}Registered fonts successfully"
+
+echo -e "${INFO}Installing paru - AUR helper"
+function install_paru() {
+  git clone https://aur.archlinux.org/packages/paru-bin paru
+  cd paru
+  makepkg -sri --noconfirm --needed
+  cd ..
+}
+install_paru || { echo -e "${FAIL}Failed to install paru"; exit 1; }
+echo -e "${DONE}Installed paru successfully"
 
 # Install packages from the AUR
 echo -e "\n${INFO}Installing AUR packages..."
@@ -48,7 +61,7 @@ while IFS= read -r line; do
       
       # Install the package
       echo "Installing $package - $comment"
-      paru -S --no-confirm -needed $package || echo -e "${FAIL}Failed to install AUR packages"
+      # paru -S --noconfirm -needed $package || { echo -e "${FAIL}Failed to install AUR packages"; exit 1; }
   fi
 done < ./lists/pkg_aur
 echo -e "${DONE}Install AUR packages successfully"
@@ -58,35 +71,38 @@ function download_xdm() {
   mkdir xdm && cd xdm
   wget https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz
   tar -xvf xdm-setup-7.2.11.tar.xz
-  sudo sh install.sh
+  chmod +x install.sh && ./install.sh
   cd ../ && sudo rm -rf xdm
 }
-download_xdm || echo -e "${FAIL}Failed to install Xtreme Download Mananger"
+download_xdm || { echo -e "${FAIL}Failed to install Xtreme Download Mananger"; exit 1; }
 echo -e "${DONE}Successfully installed Xtreme Download Manager"
 
 # Start the bluetooth service
 echo -e "\n${INFO}Starting the Bluetooth Service..."
-sudo systemctl enable --now bluetooth.service || echo -e "${FAIL}Failed to start bluetooth service"
+sudo systemctl enable --now bluetooth.service || { echo -e "${FAIL}Failed to start bluetooth service"; exit 1; }
 echo -e "${DONE}Bluetooth service started successfully"
 
 # Clean out other portals
 echo -e "\n${INFO}Cleaning out conflicting xdg portals..."
-yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk || echo -e "${FAIL}Failed to clean out conflicting xdg portals"
+paru -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk || true # Don't exit script if fail
 echo -e "${DONE}Cleaned out conflicting xdg portals successfully"
 
 # Setup ZSH
 echo -e "${INFO}Setting up ZSH..."
-mkdir -p ~/.zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-echo -e "${DONE}Set up ZSH successfully"
+function setup_zsh() {
+  mkdir -p ~/.zsh
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+}
+setup_zsh || { echo -e "${FAIL}Failed to setup zsh"; exit 1; }
+echo -e "${DONE}Setup ZSH successfully"
 
 # Change shell for root
 echo -e "${INFO}Setting shell to ZSH..."
-sudo chsh -s $(which zsh) || echo -e "${FAIL}Failed to set shell to ZSH"
+sudo chsh -s $(which zsh) || { echo -e "${FAIL}Failed to set shell to ZSH"; exit 1; }
 echo -e "${DONE}Set shell to ZSH successfully"
 
 # Symlink configurations
 echo -e "${INFO}Symlinking configurations with stow..."
-stow .
+stow . || { echo -e "${FAIL}Failed to symlink configurations";exit 1; }
 echo -e "${DONE}Symlinked configurations successfully"
